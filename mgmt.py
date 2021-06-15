efc09b602b787697695420efaeda2765004bf707
zrthstr <zrth1k@gmail.com>
2021-06-15 21:12:44 +0200

    add some omr stuff

--- mgmt.py
+++ mgmt.py
@@ -36,22 +36,60 @@ class Tor:
 
     def __init__(self, id):
         self.id = id
+
         self.file_name = self.generate_libgen_torrent_filename()
         self.full_path = self.TORRENT_DIR / self.file_name
         self.url = self.TORRENT_SRC + self.file_name
 
         ## lets check if exists in db. if not create?
-        if torrent.find_one(name=self.file_name):
-            self.exists_in_db = True
-        else:
-            self.exists_in_db = False
+        print("file_name", self.file_name)
+        ttt = torrent.find_one(file_name=self.file_name)
+        if ttt:
+            # EXISTS! lets load  TODO Rename ttt
+            print(ttt)
+            self.infohash = ttt["infohash"]
+            self.seed_count = ttt["seed_count"]
+            self.chk_fail_last = ttt["chk_fail_last"]
+            self.chk_fail_count = ttt["chk_fail_count"]
+            self.chk_success_last = ttt["chk_success_last"]
+            self.chk_success_count = ttt["chk_success_count"]
 
-        #self.is_missing = False # todo
+
+        else:
+            ## DOESNT EXCIST! lets create new
+            self.get_from_file()
+            self.save_to_db()
+            ### NOW load values?
+
+            ## TODO dedup
+            ttt = torrent.find_one(file_name=self.file_name)
+            print("file_name", self.file_name)
+            print("saved thing is now", ttt)
+            self.infohash = ttt["infohash"]
+            self.seed_count = ttt["seed_count"]
+            self.chk_fail_last = ttt["chk_fail_last"]
+            self.chk_fail_count = ttt["chk_fail_count"]
+            self.chk_success_last = ttt["chk_success_last"]
+            self.chk_success_count = ttt["chk_success_count"]
+
         #self._fetch(self)
         #self._process(self)
 
+    def info(self):
+        print(f"Info on torrent: {self.file_name}")
+        print(f"    infohash:                {self.infohash}")
+        print(f"    seed_count:              {self.seed_count}")
+        print(f"    id:                      {self.id}")
+        print(f"    full_path:               {self.full_path}")
+        print(f"    url:                     {self.url}")
+        print(f"    chk_fail_last:           {self.chk_fail_last}")
+        print(f"    chk_fail_count:          {self.chk_fail_count}")
+        print(f"    chk_success_last:        {self.chk_success_last}")
+        print(f"    chk_success_count:       {self.chk_success_count}")
+
     @staticmethod
     def newest():
+        #last = torrent.find_one(order_by='-file_name')
         last = torrent.find_one(order_by='-id')
         if last == None:
             return 0
@@ -59,28 +97,28 @@ class Tor:
             return last["id"]
 
     @staticmethod
-    def populate(count=1,col="r",only_count_absent=False):
-        newest = Tor.newest() + 1
-        for n in range(count):
-            newest += n
-            print("debug:", n, newest)
+    def populate(count=1, col="r", only_count_absent=False):
+        base = Tor.newest() + 1
+        for n in range(0,count):
+            newest = base + n
+            print("[debug] populate :", n, base, newest)
 
             if Tor.is_known_missing(newest):
                 print(".", newest)
                 continue
 
             new_tor = Tor(newest)
-            if new_tor.exists_in_db:
-                print(f"Found {new_tor.id} in db. Skipping")
-                continue
-
-            print(f"[debug] Did not find {new_tor.id} in db")
-            new_tor.get()
-            new_tor.save()
+            #if new_tor.exists_in_db:
+            #    print(f"Found {new_tor.id} in db. Skipping")
+            #    continue
+            #
+            #print(f"[debug] Did not find {new_tor.id} in db")
+            #new_tor.get_from_file()
+            #new_tor.save()
 
 
     def generate_libgen_torrent_filename(self):
-        name = self.id * 1000
+        name = (self.id - 1 ) * 1000
         return f"r_{name:03}.torrent"
 
     def get_http(self):
@@ -107,7 +145,7 @@ class Tor:
         return None, True, False
 
 
-    def get(self):
+    def get_from_file(self):
         if self.full_path.is_file():
             print(f"[debug] found f{self.full_path} in dir")
             return 'found'
@@ -156,14 +194,32 @@ class Tor:
         return [t.url for t in ti.trackers()]
 
 
-    def save(self):
-        print(f'[+] Saving torrent to DB')
+    def load(self):
+        ### load if exists, else creat new?!
+        #tor = torrent.findone(id=["self.id"])
+        tor = torrent.findone(id=["self.filename"])
+        if tor == None: ## ugly!
+            # create new
+            pass # TODO
+        else:
+            print("ihihiihihihi", tor["infohash"])
+
+
+
+
+    ##### TODO TODO TODO TODO 
+    ##### TODO TODO TODO TODO 
+    ##### TODO TODO TODO TODO 
+    def save_to_db(self):
+        print(f'[+] Saving torrent to DB from file {self.full_path} ')
         ti = lt.torrent_info(str(self.full_path))
 
         self.process_tracker(ti)
 
         infohash = str(ti.info_hashes().get_best())
-        torrent.insert(dict(name=self.file_name,
+        ##### TODO TODO TODO TODO 
+        torrent.insert(dict(file_name=self.file_name,
+                            id=self.id,
                             infohash = infohash,
                             chk_fail_count = 0,
                             chk_fail_last = None,
@@ -181,11 +237,30 @@ class Tor:
     def is_known_missing( newest, col="r"):
         return newest *1000 in config["catalogue"]["r"]["known_missing"]
 
+
+    def peer_exchange(self):
+        from ttsfix import scraper
+        all_tracker = Tracker.all()
+        print(f"all tracker: {all_tracker}")
+
+        scraper = scraper.Scraper(
+            infohashes=[self.infohash,],
+            #trackers=trackers,
+            trackers=["udp//:tracker.opentrackr.org:1337"],
+            )
+        results = scraper.scrape()
+        return results
+
+
     @staticmethod
     def peer_crawl( n=1):
         for n in range(0,n):
             oldest = torrent.find(order_by='chk_success_last').__next__() # maybe -chk..
-            print(oldest["id"])
+            print(oldest["id"], oldest["infohash"])
+
+            tor = Tor(oldest["id"])
+
+            tor.peer_exchange()
             
             # get least fresh torrent
             # try to fetch peer info
@@ -204,6 +279,12 @@ class Tor:
         ### wirte missing to config toml?
         pass
 
+class Tracker:
+    def __init__(self):
+        pass
+    @staticmethod
+    def all():
+        print(tracker.find())
 
 class Db:
     def __init__(self):
@@ -219,7 +300,7 @@ class Db:
 
         if torrent.find_one() == None:
             print("[+] init.db.torrent")
-            torrent.create_column('name', db.types.text)
+            torrent.create_column('file_name', db.types.text)
             torrent.create_column('seed_count', db.types.integer)
             torrent.create_column('chk_fail_last', db.types.datetime)
             torrent.create_column('chk_fail_count', db.types.integer)
@@ -227,7 +308,7 @@ class Db:
             torrent.create_column('chk_success_count', db.types.integer)
             torrent.create_column('infohash', db.types.text)
             #torrent.create_column('', db.types.)
-            # torrent.create_index(["name"])
+            # torrent.create_index(["file_name"])
 
         if log.find_one() == None:
             print("[+] init.db.log")
@@ -248,6 +329,8 @@ class Db:
     def integrety_chk(self):
         ### make sure no torrents are missingeg
         ### e.g. make sure that if we notice 0,1,2 and 4 but not 3
+
+        # make sure ID and file_name fitt !
         pass
 
 
@@ -263,8 +346,12 @@ config = load_config()
 #print(config)
 
 #Tor.integrety_check_libgen()
-#Tor.populate(count=160, only_count_absent=True)
-Tor.peer_crawl()
+Tor.populate(count=160, only_count_absent=True)
+#Tor.peer_crawl()
 
+
+ttt = Tor(10)
+print(ttt)
+ttt.info()
 
 
