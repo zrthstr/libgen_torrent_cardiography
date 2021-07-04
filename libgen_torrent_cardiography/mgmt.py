a62c94eb452e50af8f7f7f8cab5b520d899a1a9b
zrthstr <zrth1k@gmail.com>
2021-07-04 11:47:00 +0200

    ..

--- libgen_torrent_cardiography/mgmt.py
+++ libgen_torrent_cardiography/mgmt.py
@@ -28,9 +28,11 @@ output = Output(torrent_collection)
 #torrent_collection.populate(count=10, collection="fiction")
 
 # torrent_collection.info()
-# torrent_collection.peer_crawl(1)
+torrent_collection.peer_crawl(1)
+
 
 for loop in range(10):
+    break
     for collection in ["books", "scimag", "fiction"]:
         #torrent_collection.populate(count=30, collection=collection)
         torrent_collection.peer_crawl(10)
