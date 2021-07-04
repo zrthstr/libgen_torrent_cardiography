1aff28ab540aec977e5712f8b2c8bd0191ab35f3
zrthstr <zrth1k@gmail.com>
2021-07-04 13:58:29 +0200

    ..

--- libgen_torrent_cardiography/mgmt.py
+++ libgen_torrent_cardiography/mgmt.py
@@ -1,4 +1,5 @@
 from database import Database
+from datetime import datetime, timedelta
 
 from utils import load_config
 from torrent_collection import Torrent_collection
@@ -6,10 +7,22 @@ from output import Output
 
 CONFIG = "config/mgmt.toml"
 config = load_config(CONFIG)
-
 db = Database(config)
 # db.integrety_chk()
 # db.info()
+
+def run_for(minutes):
+    end = datetime.now() + timedelta(minutes=minutes)
+    while end < datetime.now():
+        run()
+
+
+def run()
+    for loop in range(100):
+        for collection in ["books", "scimag", "fiction"]:
+            torrent_collection.populate(count=30, collection=collection)
+            torrent_collection.peer_crawl(1)
+
 
 torrent_collection = Torrent_collection(db, config)
 output = Output(torrent_collection)
@@ -30,10 +43,6 @@ output = Output(torrent_collection)
 # torrent_collection.info()
 # torrent_collection.peer_crawl(1)
 
-
-for loop in range(100):
-    for collection in ["books", "scimag", "fiction"]:
-        torrent_collection.populate(count=30, collection=collection)
-        torrent_collection.peer_crawl(1)
+run_for(4)
 
 output.generate()
